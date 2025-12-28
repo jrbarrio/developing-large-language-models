c0d17f842c6b806b189d8bf670f6232c60ee0800
Jorge Roldan <jorge.roldan@gmail.com>
2025-12-28 09:36:59 +0100

    Methods for high-quality feedback gathering

---
+++ 08-reinforcement-learning-from-human-feedback/2-gathering-human-feedback/src/1.2-comparing-slogans-for-a-gym-campaign.py
@@ -0,0 +1,14 @@
+def evaluate_slogans(slogans_X, slogans_Y):
+    wins_X, wins_Y = 0, 0
+    for (slogan_X, score_X), (slogan_Y, score_Y) in zip(slogans_X, slogans_Y):
+        # Assign one point to X if score X is higher, otherwise to Y
+        if (score_X > score_Y):
+            wins_X += 1
+        else:
+            wins_Y += 1
+    success_rate_X = (wins_X / len(slogans_X)) * 100
+    success_rate_Y = (wins_Y / len(slogans_Y)) * 100
+    return success_rate_X, success_rate_Y
+
+results = evaluate_slogans(slogans_X, slogans_Y)
+print(f"The resulting scores are {results}")
